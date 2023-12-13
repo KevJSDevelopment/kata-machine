5a681a0c96aff3b2adb2619b5ad5914c446fcfd7
KevJSDev <KevJSDev@users.noreply.github.com>
2023-12-12 22:23:15 -0500

    doubly linked list

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,36 @@
-export default function quick_sort(arr: number[]): void {
+function partition (arr: number[], low: number, high: number): number {
 
+    const pivot = arr[high];
+    let idx = -1;
+
+    for(let i = low; i < high; i++){
+        if(arr[i] < pivot){
+            idx++;
+            const temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+    }
+
+    idx++
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+function qs (arr: number[], low: number, high: number): void {
+
+    if(low >= high){
+        return;
+    }
+
+    const pivotIdx = partition(arr, low, high);
+
+    qs(arr, low, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, high);
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
