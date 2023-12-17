90245330bb569d246e4be150322a445a371ba27e
KevJSDev <KevJSDev@users.noreply.github.com>
2023-12-17 15:42:23 -0500

    minheap done

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
+
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
+    const q = [head];
+
+    while (q.length > 0) {
+
+        const next = q.shift() as BinaryNode<number> | undefined | null;
+        if(!next) continue;
+        // search
+        if(next?.value === needle) {
+            return true;
+        }
+
+        if(next?.left) q.push(next.left);
+        if(next?.right) q.push(next.right);
+    }
+
+    return false;
 }
